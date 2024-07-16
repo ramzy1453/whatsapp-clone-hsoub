3900d7ee56a8426ec1edd9c88c6c764d368d5a41
ramzy1453 <kemmounramzy93@gmail.com>
2024-07-16 16:56:34 +0100

    change lowercase

--- server/middlewares/multer.js
+++ server/middlewares/multer.js
@@ -1,5 +1,6 @@
 import multer from "multer";
 import path from "path";
+
 const storage = multer.diskStorage({
   destination: "public/uploads/",
   filename: (req, file, cb) => {
@@ -11,12 +12,12 @@ const upload = multer({
   limits: { fileSize: 1024 * 1024 },
   storage: storage,
   fileFilter: (req, file, cb) => {
-    const filetypes = /jpeg|jpg|png/;
-    const mimetype = filetypes.test(file.mimetype);
-    const extname = filetypes.test(
+    const fileTypes = /jpeg|jpg|png/;
+    const mimeType = fileTypes.test(file.mimetype);
+    const extname = fileTypes.test(
       path.extname(file.originalname).toLowerCase()
     );
-    if (mimetype && extname) {
+    if (mimeType && extname) {
       return cb(null, true);
     }
     cb(new Error("Only images are allowed"));
